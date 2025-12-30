7a43ae386e1cb9b112b2281209383cde46a3afce
ayyyy/ <ack159ack@gmail.com>
2025-12-30 16:48:32 +0800

    Update model.go

--- config/model.go
+++ config/model.go
@@ -2,13 +2,36 @@ package config
 
 var ModelReverseMap = map[string]string{}
 var ModelMap = map[string]string{
+	"claude-4-sonnet":         "claude2",
+	"claude-4-sonnet-think":   "claude37sonnetthinking",
 	"claude-4-5-sonnet":       "claude45sonnet",
 	"claude-4.5-sonnet-think": "claude45sonnetthinking",
+	"gemini-2.5-pro":          "gemini25pro",
 	"gemini-3-pro":            "gemini30pro",
+	"kimi-k2-thinking":        "kimik2thinking",
+	"grok":                    "grok",
+	"grok-4":                  "grok4",
+	"grok-4-non-thinking":     "grok4nonthinking",
+    "grok-4.1-reasoning":      "grok41reasoning",
+	"grok-4.1-non-reasoning":  "grok41nonreasoning",
 	"o3-pro":                  "o3pro",
+	"o4-pro":                  "o4mini",
+	"gpt-4o":                  "gpt4o",
+	"gpt-4.1":                 "gpt41",
 	"gpt-5.1":                 "gpt51",
 	"gpt-5-think":             "gpt5_thinking",
+	"claude-4-opus":           "claude40opus",
+	"claude-4-opus-think":     "claude40opusthinking",
+	"claude-4.1-opus":         "claude41opus",
 	"claude-4.1-opus-think":   "claude41opusthinking",
+	"claude-4.5-opus":         "claude45opus",
+	"claude-4.5-opus-think":   "claude45opusthinking",
+	"alpha":                   "pplx_alpha",
+	"beta":                    "pplx_beta",
+	"study":                   "pplx_study",
+	"r1":                      "r1",
+    "claude40sonnetthinking-labs":   "claude40sonnetthinking_labs",
+	"claude40opusthinking-labs":   "claude40opusthinking_labs",
 }
 var MaxModelMap = map[string]string{
 	"o3-pro":                "o3pro",
@@ -66,3 +89,4 @@ func buildResponseModels() {
 		})
 	}
 }
+
